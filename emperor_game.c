952c2a39b6d901a1c9494932b128e171d407902c
Gridmerc <58649232+Gridmerc@users.noreply.github.com>
2021-06-11 15:22:36 +0800

    Create emperor_game.c

---
+++ emperor_game.c
@@ -0,0 +1,18 @@
+/*
+	AUTHOR: JATICO II, NILO CANTIL K.
+*/
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+
+typedef char String[10];
+
+int main() {
+	int round = 1;
+	int mmLeft = 30;
+	int moneyEarned = 0;
+	
+	return 0;
+}
